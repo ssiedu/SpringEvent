bd16d5ec7d5c5defe0dc5859063d8973ab692081
manoj <manojs121@rediffmail.com>
2019-03-13 15:09:27 +0530

    13-03-19

--- src/main/java/com/ssi/Client.java
+++ src/main/java/com/ssi/Client.java
@@ -1,20 +1,18 @@
 package com.ssi;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//BeanFactory factory=new XmlBeanFactory(new FileSystemResource("e:\\conf\\spring.xml"));
-		//BeanFactory factory=new XmlBeanFactory(new ClassPathResource("spring.xml"));
-		//Test t=(Test)factory.getBean("myObj");
-		//ApplicationContext context=new FileSystemXmlApplicationContext("e:\\conf\\spring.xml");
-		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+	
+		ConfigurableApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		context.start();
 		Test t=(Test)context.getBean("myObj");
 		t.show();
+		context.stop();
+		
 	}
 
 }
